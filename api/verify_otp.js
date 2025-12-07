f85bd9b32d0c335bb2d50f1da076f13ddf01fd26
nguyenbaolong06072004-droid <nguyenbaolong06072004@gmail.com>
2025-12-07 18:08:32 +0700

    Update verify_otp.js

--- api/verify_otp.js
+++ api/verify_otp.js
@@ -1,17 +1,17 @@
 export default function handler(req, res) {
     const { otp } = req.query;
 
-    if (!global.currentOtp) {
-        return res.status(400).json({ success: false, msg: "Chưa tạo OTP" });
+    if (!global.currentOtp || !global.otpExpire) {
+        return res.status(400).json({ error: "NO_OTP_CREATED" });
     }
 
     if (Date.now() > global.otpExpire) {
-        return res.status(400).json({ success: false, msg: "OTP hết hạn" });
+        return res.status(400).json({ error: "OTP_EXPIRED" });
     }
 
     if (otp == global.currentOtp) {
         return res.status(200).json({ success: true, msg: "OTP đúng" });
     }
 
-    return res.status(400).json({ success: false, msg: "Sai OTP" });
+    res.status(400).json({ success: false, msg: "OTP sai" });
 }
